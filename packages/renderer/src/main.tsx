69f035847c22c8fe6a3e5511b230ff0b962177b4
Joseph Mearman <joseph@mearman.co.uk>
2026-05-12 21:15:49 +0100

    fix: remove mosaic CSS import from main.tsx

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
-import "react-mosaic-component/react-mosaic-component.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
